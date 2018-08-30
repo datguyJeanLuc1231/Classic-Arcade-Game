88e383c372f4f9d9f401b89b6cf2037d6362a533
Jean-Luc-Adams <jladams91@gmail.com>
2018-08-30 12:04:08 -0500

    Adding collision properties to the game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+// Hero class: the player's position and movement methods
 class Hero {
     constructor() {
         this.sprite = 'images/char-boy.png';
@@ -7,11 +9,10 @@ class Hero {
         this.y = 0;
         // The starting position coordinates for the hero
         this.startX = this.xJump * 2;
-        this.startY = (this.yJump * 5) - 20; // The subtracting 20 adds some padding for the hero
+        this.startY = (this.yJump * 4) + 60; // The subtracting 20 adds some padding for the hero
         this.x = this.startX;
         this.y = this.startY;
     }
-
     // Creating hero on the starting position
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -42,6 +43,14 @@ class Hero {
                 break;
         }
     }
+    // Checks for collision on the x axis and y-axis
+    update() {
+        for(let enemy of allEnemies) {
+            if (this.y === enemy.y && (enemy.x + enemy.xJump/2 > this.x && enemy.x < this.x + this.xJump/2) ) {
+                this.reset();
+            }
+        }
+    }
 }
 
 // Enemies our player must avoid
@@ -55,7 +64,7 @@ let Enemy = function(x, y, speed) {
     this.resetPosition = -this.xJump;
 };
 
-// Initialize the player and enemy objects
+// Initialize the player and enemy, while giving the enemies various starting points and speeds
 const player = new Hero(),
     bug1 = new Enemy(-101, 0, 200),
     bug2 = new Enemy(-101, 83, 85),
@@ -66,8 +75,6 @@ const player = new Hero(),
     allEnemies = [];
 allEnemies.push(bug1, bug2, bug3, bug4, bug5, bug6);
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // If enemy is not passed boundary
     if(this.x < this.outOfBounds) {
@@ -86,17 +93,6 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- js/engine.js
+++ js/engine.js
@@ -93,7 +93,7 @@ let Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
-        // player.update();
+        player.update();
     }
 
     /* This function initially draws the "game level", it will then call
